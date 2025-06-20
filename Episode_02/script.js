f34567b808fa54273433d77e346ae6d7758db1ed
Guda Manjunadha <gudamanjunadhareddy@gmail.com>
2025-06-20 09:16:50 +0530

    changes

--- Episode_02/script.js
+++ Episode_02/script.js
@@ -61,11 +61,11 @@
 gsap.to("#page2 h1",{
     transform: "translateX(-150%)",
     scrollTrigger: {
-        trigger: "#page2 h1",
+        trigger: "#page2 ",
         scroller: "body",
         markers: true,
-        start: "top 0%",
-        end: "top -150%",
+        start: "top 0",
+        end: "top -125%",
         scrub: 2,
         pin: true,
     }
